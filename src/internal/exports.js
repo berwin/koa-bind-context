568974181919bb145137466ccbe86428b5f2501b
berwin <berwin1995@qq.com>
2016-05-24 19:32:11 +0800

    添加注释，优化代码

--- src/internal/exports.js
+++ src/internal/exports.js
@@ -18,26 +18,38 @@ var util = require('../util/index.js');
 module.exports = function (BindContext) {
   var bc = BindContext.prototype;
 
+  /*
+   * 暴露api
+   * 针对入口文件（config.main这个模块）抛出的方法进行模拟，并抛出具有相同方法的api
+   * 不同的是，外界操作所抛出的方法时，咱们会做一些处理（绑定上下文），然后在执行入口文件所抛出的方法
+   * 
+   * @return {Object || Function}
+   */
   bc._exports = function () {
     var toString = Object.prototype.toString;
     var entranceModule = this._entrance.module;
     var type = toString.call(entranceModule);
 
     if (type === '[object Object]') {
-
-      var _exports = {};
-
+      var exports = {};
       for (var i in entranceModule) {
-        _exports[i] = this._dispath(entranceModule[i]);
+        exports[i] = this._dispath(entranceModule[i]);
       }
-
-      return _exports;
+      return exports;
     }
 
     if (type === '[object Function]') {
       return this._dispath(this._entrance.module);
     }
   };
+
+  /*
+   * 返回一个中间件
+   * 执行中间件将会把上下文绑定到依赖抛出的方法，并且执行该方法
+   * 
+   * @return {Function}
+   * @api private
+   */
 
   bc._dispath = function (fn) {
     var self = this;
