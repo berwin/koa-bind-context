e2cd26cde14199b1a99986d36248fa95438b6a43
berwin <berwin1995@qq.com>
2016-05-24 11:44:11 +0800

    添加注释

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,30 @@ module.exports = function (BindContext) {
   var bc = BindContext.prototype;
 
   /**
-   * Set the modules dependencies so that the modules can access the context
+   * 设置模块的依赖关系，使所有配置中的模块都可以访问上下文
+   *
+   * @例子
+   *
+   * project - controllers/user/index.js
+   *
+   * ```javascript
+   * koaBindContext.config({
+   *   main: {
+   *     controller: './user.js'
+   *   },
+   *   context: {
+   *     service: '../../service/user/index.js',
+   *     proxy: '../../proxy/user/index.js'
+   *   }
+   * });
+   * ```
+   *
+   * 上面的例子中，'../../service/user/index.js'，'../../proxy/user/index.js'，controllers/user/user.js 这三个文件共用同一个context
+   *
+   * @param {Object} config 配置文件有两个必填字段 main 与 context 
+   * @param {Object} config.main 必选 自定义 key 与 value
+   * @param {Object} config.main key 可以在 context 中通过这个 key 来读取对应的模块
+   * @param {Object} config.main value 配置模块地址，根据模块路径把模块抛出的内容填入 context
    */
 
   bc.config = function (config) {
@@ -21,7 +44,18 @@ module.exports = function (BindContext) {
   };
 
   /**
-   * Expose `exports`.
+   * 暴露出口
+   * 模拟“入口”抛出的API，使其暴露的 API 与 config中配置的 main 入口文件中所抛出的接口一模一样~
+   *
+   * @例子
+   *
+   * project - controllers/user/index.js
+   *
+   * ```javascript
+   * koaBindContext.exports();
+   * ```
+   *
+   * @return {Object || Function || Other} 返回内容与入口文件的 module.exports 相同~
    */
   bc.exports = function () {
     return this._exports();
