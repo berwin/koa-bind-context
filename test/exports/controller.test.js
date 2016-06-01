61674037ce7e6a28561e482b8aabd11af93c73cd
berwin <berwin1995@qq.com>
2016-06-01 16:50:06 +0800

    添加注释

--- test/exports/controller.test.js
+++ test/exports/controller.test.js
@@ -1,5 +1,15 @@
+/**!
+ * Koa-bind-context - test/exports/controller.test.js
+ *
+ * Unit test
+ *
+ * Authors:
+ *  Berwin <liubowen.niubi@gmail.com> (https://github.com/berwin)
+ */
+
+'use strict';
+
 exports.test = function *() {
   var result = yield *this.testService.test();
-
   if (result !== 'test context') throw new Error('Module dependencies can`t be able to access context')
 };
--- test/exports/service.test.js
+++ test/exports/service.test.js
@@ -1,3 +1,14 @@
+/**!
+ * Koa-bind-context - test/exports/service.test.js
+ *
+ * Unit test
+ *
+ * Authors:
+ *  Berwin <liubowen.niubi@gmail.com> (https://github.com/berwin)
+ */
+
+'use strict';
+
 exports.test = function *() {
   return this.testStr;
 };
